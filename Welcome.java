2447904031f7b0eb9f6e2880873f43442b7ab5da
Gitea <gitea@fake.local>
2023-07-19 06:14:00 +0000

    v3

--- Welcome.java
+++ Welcome.java
@@ -2,5 +2,6 @@ public class Welcome{
 	public static void main(String[] args){
 		System.out.println("Welcome to Version control");
 		System.out.println("Modified - V2");
+		System.out.println("Again revised - V3");
 	}
 }
